997777f7c7943342065c4dfea1829af4a7647037
DavSan <david.sanchez.martinez@outlook.com>
2024-08-06 13:55:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const itemList = document.getElementById('itemList');
 let array = []; // This array will store elements to be manipulated
 
 // Step 3: Implementing Array Methods
+// Step 4: Retrieve an element from an input field and add it to the end of the array
 function addElement() {
-
+    const elementInput = itemInput.value.trim();
+    array.push(elementInput);
 } 
+
+
