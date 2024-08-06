d1c08bb66c992bd27c5c99366d9378dac74a1b32
DavSan <david.sanchez.martinez@outlook.com>
2024-08-06 13:36:45 +0100

    Update index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+// Step 1: Get references to the input field, button, and item list
+
+
+
+
+// Step 2: Initialize the Array
+let array = []; // This array will store elements to be manipulated
+
+// Step 3: Implementing Array Methods
+function addElement() {
+
+} 
