836a1a130494698d588df1fede4ac998e233e90f
DavSan <david.sanchez.martinez@outlook.com>
2024-08-06 13:40:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 // Step 1: Get references to the input field, button, and item list
 const itemInput = document.getElementById('itemInput');
 const itemButton = document.getElementById('addItemButton');
-
-
+const itemList = document.getElementById('itemList');
 
 // Step 2: Initialize the Array
 let array = []; // This array will store elements to be manipulated
